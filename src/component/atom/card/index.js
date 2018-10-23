e9d2e4567f5f97212646b0d26056465097f6c6f9
artemiy.troshkin <artemiy.troshkin@mail.ru>
2018-10-23 16:32:57 +0300

    release v3

--- src/component/atom/card/index.js
+++ src/component/atom/card/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './card.css';
+import { data } from '../../../data';
 
 export default class Catalog extends Component {
   render() {
@@ -7,10 +8,12 @@ export default class Catalog extends Component {
     
       <div className="card">
       	 <div className="image">
-		 <img src="img/VineBotle.jpg" alt="Изображение" title="Изображение" />
+		 <img src={this.props.data.picture} alt="Изображение" title="Изображение" />
 		 </div>
-            <h1>Chardonnay Sauvignon</h1>
-			<p>Year: 1999</p>
+     <h1>Chardonnay Sauvignon</h1>
+     {/* <h1>{this.props.data.name}</h1> */}
+     <h2>{this.props.data.price}</h2>
+      <p>Year: {this.props.data.year}</p>
        <button className="button_card">BUY</button>
       </div>
     
