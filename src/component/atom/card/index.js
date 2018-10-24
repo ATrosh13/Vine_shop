b47f47f735167847ff866a73ee10fbf1352e2372
artemiy.troshkin <artemiy.troshkin@mail.ru>
2018-10-24 12:23:37 +0300

    release v3.1

--- src/component/atom/card/index.js
+++ src/component/atom/card/index.js
@@ -10,8 +10,8 @@ export default class Catalog extends Component {
       	 <div className="image">
 		 <img src={this.props.data.picture} alt="Изображение" title="Изображение" />
 		 </div>
-     <h1>Chardonnay Sauvignon</h1>
-     {/* <h1>{this.props.data.name}</h1> */}
+     {/* <h1>Chardonnay Sauvignon</h1> */}
+     <h1>{this.props.data.name}</h1>
      <h2>{this.props.data.price}</h2>
       <p>Year: {this.props.data.year}</p>
        <button className="button_card">BUY</button>
